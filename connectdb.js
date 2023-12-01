aa2b18b336b91fd0633b616ae4145d29a0816905
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-01 17:01:53 -0500

    added different way of connecting sequelize to postgres

--- connectdb.js
+++ connectdb.js
@@ -1,8 +1,19 @@
 const { Sequelize } = require('sequelize');
 
+const { dbName, dbUser, dbPass, dbPort } = require('./configdb');
+
+
+// Issue: Doesn't seem to work on other computers
 // Instantiate Sequelize, connect to postgres database
 // Right now it doesn't create a database, it uses one created prior.
-let dbName = 'todoList';
-const db = new Sequelize('postgres://localhost/' + dbName);
+// let dbName = 'todoList';
+// const db = new Sequelize('postgres://localhost/' + dbName);
+
+// New test
+const db = new Sequelize(dbName, dbUser, dbPass, {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: dbPort
+  });
 
 module.exports = { db };
