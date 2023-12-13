245285d53f7327fe4476a3bc40c24332d438f7ea
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-13 17:20:32 -0500

    remove fields that will be left to default

--- index.js
+++ index.js
@@ -23,15 +23,12 @@ app.get("/getAllEntries", async (req, res) => {
 });
 
 app.post("/createEntry", async (req, res) => {
-  let entryText = req.body;
+  let entry = req.body;
   //console.log(entryText);
   const createEntry = Entry.create({
-    completed: entry.completed,
     text: entry.text,
     priority: entry.priority,
     list_id: entry.list_id,
-    entry_id: entry.entry_id,
-    color: entry.color,
   });
 
   res.send("created entry");
